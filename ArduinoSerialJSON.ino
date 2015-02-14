b1f3395e8958cdbddf6c571eb482009267bf62e7
Eduard Paraschivescu <ed.orangeduke@gmail.com>
2015-02-14 15:37:50 -0500

    Serial communication started

--- ArduinoSerialJSON.ino
+++ ArduinoSerialJSON.ino
@@ -4,9 +4,10 @@ const int sensors[] = {A0, A1, A2, A3, A4, A5};
 int values[] = {0, 0, 0, 0, 0, 0};
 
 setup(){
-
+  Serial.begin(9600);
 }
 
 loop(){
-  
+  Serial.write("Hello World!");
+  delay(2000);
 }
